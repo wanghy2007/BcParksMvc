7e682486144ce630d5ec26900927f47cc20ac86d
Hong-Yi Wang <wanghy2007@gmail.com>
2022-07-22 15:43:47 -0700

    Fix column headings

--- Models/ParkType.cs
+++ Models/ParkType.cs
@@ -5,6 +5,8 @@ namespace BcParksMvc.Models
     public class ParkType
     {
         public int Id { get; set; }
+
+        [Display(Name = "Park type")]
         public string Name { get; set; }
         public string Abbreviation { get; set; }
 
