ea67a811be81aa565838b56314efe74840d31754
Hong-Yi Wang <wanghy2007@gmail.com>
2022-07-22 23:44:22 -0700

    Add pagination functionality for Park

--- Models/Park.cs
+++ Models/Park.cs
@@ -6,16 +6,12 @@ namespace BcParksMvc.Models
     {
         public int Id { get; set; }
 
-        [Display(Name = "Park name")]
         public string Name { get; set; }
 
-        [Display(Name = "Area (ha)")]
         public int AreaHa { get; set; }
 
-        [Display(Name = "Area (acres)")]
         public int AreaAcres { get; set; }
 
-        [Display(Name = "Established")]
         public int EstablishedYear { get; set; }
 
         public ICollection<ParkType> ParkTypes { get; set; }
